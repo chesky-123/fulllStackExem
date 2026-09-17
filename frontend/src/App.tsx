059661a0205130f0b897522202ac3ef619bf11a2
chesky <sye0534184736@gmail.com>
2026-09-17 17:47:44 +0300

    fix ProtectedRoutes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,27 @@ import { Route, Routes } from "react-router";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 function App() {
 
   return (
    <Routes>
-    <Route path="/register" element={<Register/>}/>
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/profil" element={<Profile/>}/>
-    <Route path="*" element={<div>404 not found</div>}/>
+    <Route path="/login" element={
+          <ProtectedRoutes isAuthRequired={false}>
+            <Login />
+          </ProtectedRoutes>
+        } />
+        <Route path="/register" element={
+          <ProtectedRoutes isAuthRequired={false}>
+            <Register />
+          </ProtectedRoutes>
+        } />
+        <Route path="/profile" element={
+          <ProtectedRoutes>
+            <Profile />
+          </ProtectedRoutes>
+        } />
    </Routes>
   )
 }
